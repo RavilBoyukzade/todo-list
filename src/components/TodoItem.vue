05ff382cf2f1c6b0d1eb4e977cce2b66560eab8c
Ravil <34988095+RavilBoyukzade@users.noreply.github.com>
2022-03-23 20:20:09 +0400

    TodoItem delete button style add

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -6,7 +6,7 @@
            <strong>{{todo.id}}</strong>
            {{todo.title}}
        </span>
-       <button>&times;</button>
+       <button class="btn-delete">&times;</button>
     </li>
   </div>
 </template>
@@ -30,11 +30,26 @@ export default {
     margin-bottom: 10px;
     padding: 10px 20px;
   }
-  button{
-    color: brown;
-    border-radius: 30px;
-    border:white;
-    background-color: white;
+.btn-delete{
+    color: #dc3545;
+    background-color: transparent;
+    background-image: none;
+    border-color: #dc3545;
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    user-select: none;
+    border: 1px solid ;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: 50px;
+  }
+  .btn-delete:hover{
+    background-color: #dc3545;
+    color:#fff;
   }
 
 </style>
